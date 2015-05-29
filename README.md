604cf6675c068b0ac0adf7430644e42aea65ea8a
Mike Staszel <crimsonredmk@gmail.com>
2015-05-29 15:04:19 -0500

    add README and make it all work

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # go-dyn-dns #
 
 Tool to update AWS Route53 with an EC2 instance's public IP address.
+
+# Configuration #
+
+Create a configuration file at `/etc/go-dyn-dns-conf.json` with contents:
+
+    {
+        "AWS_ACCESS_KEY": "YOUR_ACCESS_KEY",
+        "AWS_SECRET_ACCESS_KEY": "YOUR_SECRET_ACCESS_KEY",
+        "HOSTED_ZONE_ID": "YOUR_HOSTED_ZONE_ID",
+        "SUBDOMAIN": "mysubdomain.mydomain.com"
+    }
--- go-dyn-dns.go
+++ go-dyn-dns.go
@@ -14,7 +14,7 @@ import (
 type config struct {
 	AWS_ACCESS_KEY string
 	AWS_SECRET_ACCESS_KEY string
-	ZONE_NAME string
+	HOSTED_ZONE_ID string
 	SUBDOMAIN string
 }
 
@@ -40,12 +40,21 @@ func getPublicIP() ([]byte, error) {
 	return ioutil.ReadAll(res.Body)
 }
 
-func connectToRoute53() (*route53.Route53, error) {
+func connectToRoute53(AWS_ACCESS_KEY string, AWS_SECRET_ACCESS_KEY string) (*route53.Route53, error) {
 	auth := aws.Auth{
-		AccessKey: "ASDFASDFASDFASDK",
-		SecretKey: "DSFSDFDWESDADSFASDFADFDSFASDF",
+		AccessKey: AWS_ACCESS_KEY,
+		SecretKey: AWS_SECRET_ACCESS_KEY,
 	}
 	return route53.NewRoute53(auth)
+}
+
+func updateRoute53Record(publicIP string, configMap *config, route53Client *route53.Route53) (*route53.ChangeResourceRecordSetsResponse, error) {
+	rrv := []route53.ResourceRecordValue{route53.ResourceRecordValue{Value: publicIP}}
+	basicRRS := route53.BasicResourceRecordSet{Action: "UPSERT", Name: configMap.SUBDOMAIN, Type: "A", TTL: 60, Values: rrv}
+	RRSSlice := []route53.ResourceRecordSet{route53.ResourceRecordSet(basicRRS)}
+	createRequest := route53.ChangeResourceRecordSetsRequest{Changes: RRSSlice, Xmlns: "https://route53.amazonaws.com/doc/2013-04-01/"}
+	
+	return route53Client.ChangeResourceRecordSet(&createRequest, configMap.HOSTED_ZONE_ID)
 }
 
 func logErrorThenExit(err error) {
@@ -60,11 +69,15 @@ func main() {
 	logErrorThenExit(err)
 	fmt.Printf("%s\n", publicIP)
 
-	route53Client, err := connectToRoute53()
+	configMap, err := readConfigFile()
+	logErrorThenExit(err)
+	fmt.Printf("%s\n", configMap.HOSTED_ZONE_ID)
+
+	route53Client, err := connectToRoute53(configMap.AWS_ACCESS_KEY, configMap.AWS_SECRET_ACCESS_KEY)
 	logErrorThenExit(err)
 	fmt.Printf("%s\n", route53Client.Endpoint)
 
-	configMap, err := readConfigFile()
+	_, err = updateRoute53Record(string(publicIP), configMap, route53Client)
 	logErrorThenExit(err)
-	fmt.Printf("%s\n", configMap.ZONE_NAME)
+	fmt.Println("All done!")
 }
